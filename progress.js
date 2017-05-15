83937329a886c5d147ef6749e3878ee6de2ba6c3
Валера Любимов <v.liubimov@pugofka.com>
2017-05-15 13:47:06 +0300

    Js circle timer

--- progress.js
+++ progress.js
@@ -1,23 +1,42 @@
-// Const
-var color = '#cccccc';
-var time = 5; //seconds
-
+// Canvas
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var cw = canvas.width;
 var ch = canvas.height;
 
-ctx.beginPath();
-ctx.lineWidth = cw * .04;
-ctx.strokeStyle = color;
-ctx.arc(cw/2, ch/2, cw * .45, 0, Math.PI+(Math.PI*1)/2, true);
-ctx.stroke();
+// Const
+var color = '#cccccc';
+var time = canvas.getAttribute('data-time'); //seconds
 
-// Countdown
-var countdown = setInterval(function () {
-}, 1000);
+// Interval
+var interval = 2/time;
+var start = interval;
 
-// Stop countdown
-setTimeout(function() {
-	clearInterval(countdown);
-}, time*1000);
+function init () {
+	ctx.beginPath();
+	ctx.lineWidth = cw * .04;
+	ctx.strokeStyle = color;
+	ctx.arc(cw/2, ch/2, cw * .45, 0, 2*Math.PI, true);
+	ctx.stroke();
+
+	// Countdown
+	var countdown = setInterval(function () {
+		draw(start*Math.PI);
+		start += interval;
+	}, 1000);
+	
+	// Stop countdown
+	setTimeout(function() {
+		clearInterval(countdown);
+		draw(0*Math.PI);
+	}, time*1000);
+}
+
+function draw(end) {
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.beginPath();
+	ctx.arc(cw/2, ch/2, cw * .45, 0, end, true);
+	ctx.stroke();
+}
+
+init ();
